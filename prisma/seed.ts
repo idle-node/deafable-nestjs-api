97ba6aefa5542aa658082a865993562cb8ce7b3f
moanrisy <moanrisy@gmail.com>
2022-07-21 17:31:32 +0700

    add faker-js seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+// prisma/seed.ts
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import * as dotenv from 'dotenv';
+import * as argon from 'argon2';
+
+const prisma = new PrismaClient();
+
+const fakerFirstUser = (): any => ({
+  firstName: 'test',
+  lastName: 'user',
+  email: 'test@example.com',
+});
+
+const fakerUserWithoutHash = (): any => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+});
+
+async function main() {
+  const fakerRounds = 10;
+  dotenv.config();
+  console.log('Seeding...');
+  /// --------- Users ---------------
+  for (let i = 0; i < fakerRounds; i++) {
+    // first user always have same value
+    const fakerUser =
+      i == 0
+        ? fakerFirstUser()
+        : fakerUserWithoutHash();
+
+    const hash = await argon.hash(
+      i == 0
+        ? 'test12'
+        : faker.internet.password(),
+    );
+
+    fakerUser.hash = hash;
+
+    await prisma.user.create({
+      data: fakerUser,
+    });
+  }
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
